e33d023028e84b5edb6189cb0a737d5c79a777e7
XC3S <m.geithe@gmx.de>
2020-06-27 16:29:00 +0200

    seperated request scopes / added params to url

--- index.js
+++ index.js
@@ -2,71 +2,108 @@ var express = require('express')
 var app = express()
 var sharp = require('sharp');
 var fs = require('fs');
-var request = require('request');
 
-var download = function(uri, filename, callback){
-    request.head(uri, function(err, res, body){
-        console.log('content-type:', res.headers['content-type']);
-        console.log('content-length:', res.headers['content-length']);
-
-        request(uri).pipe(fs.createWriteStream(filename)).on('close', callback);
-    });
-};
+var http = require('http');
+var https = require('https');
+var crypto = require('crypto')
 
 // respond with "hello world" when a GET request is made to the homepage
-app.get('/', function (req, res, next) {
-    //res.send('hello world')
-    var options = {
-        dotfiles: 'deny',
-        headers: {
-            'x-timestamp': Date.now(),
-            'x-sent': true
+app.get('/:x/:y/:url', function (req, res, next) {
+    var optimization = {
+        x: parseInt(req.params.x),
+        y: parseInt(req.params.y),
+        url: decodeURI(req.params.url)
+    }
+
+    var icarus = new Icarus(optimization,req, res, next);
+})
+
+class Icarus {
+    constructor(optimization,req, res, next){
+        this.optimization = optimization;
+        this.req = req;
+        this.res = res;
+        this.next = next;
+
+        this.filename = crypto.createHash('sha256').update(JSON.stringify(optimization)).digest('hex');
+        this.fullfilename = this.filename + '.jpeg';
+        this.tempfilename = this.filename + '.jpg'; // @TODO: dynamic file ending
+
+        this.options = {
+            dotfiles: 'deny',
+            headers: {
+                'x-timestamp': Date.now(),
+                'x-sent': true
+            }
+        }
+
+        this.process()
+    }
+
+    process(){
+        if(!fs.existsSync(__dirname + '/images/' + this.fullfilename)){
+            this.download(this.optimization.url,  __dirname + '/tmp/' +  this.tempfilename,() => {
+                this.optimize(() => {
+                    var filepath = __dirname + "/images/" + this.fullfilename;
+                    this.res.sendFile(filepath, this.options, function (err) {
+                        if (err) {
+                            this.next(err)
+                        } else {
+                            console.log('Sent:', filepath)
+                        }
+                    })
+                })
+            })
+        }
+        else {
+            this.sendCached()
         }
     }
 
-    var item ='Test2.jpg';
-    if(!fs.existsSync(__dirname + '/images/' + item.replace(/\.[^.]+$/, '.jpeg'))){
-        download('https://pbs.twimg.com/media/EYw8qSLWoAEf7YK?format=jpg&name=small',  __dirname + '/tmp/' + item,() => {
-            console.log('downloaded file')
-            sharp( __dirname + '/tmp/' + item)
-            .resize(100,100)
-            .removeAlpha()
-            .jpeg({ 
-                quality: 75,
-                progressive: true
-            })
-            .toFile(__dirname + '/images/' + item.replace(/\.[^.]+$/, '.jpeg'), (err, info) => {
-                console.log('optimized file') 
-                if(err){
-                    console.error('[ERROR]', err);
-                }
-                else {
-                    var fileName = __dirname + "/images/" + item.replace(/\.[^.]+$/, '.jpeg');
-                    res.sendFile(fileName, options, function (err) {
-                        if (err) {
-                            next(err)
-                        } else {
-                            console.log('Sent:', fileName)
-                            fs.unlink(__dirname + '/tmp/' + item,() => {})
-                        }
-                    })
-                }
-            });
-
+    optimize(callback){
+        sharp( __dirname + '/tmp/' + this.tempfilename)
+        .resize(this.optimization.x,this.optimization.y)
+        .removeAlpha()
+        .jpeg({ 
+            quality: 75,
+            progressive: true
         })
+        .toFile(__dirname + '/images/' + this.fullfilename, (err, info) => {
+            console.log('optimized file') 
+            if(err){
+                console.error('[ERROR]', err);
+            }
+            else {
+                fs.unlink(__dirname + '/tmp/' + this.tempfilename,() => {})
+                callback()
+            }
+        });
     }
-    else {
-        console.log('cached file');
-        var fileName = __dirname + "/images/" + item.replace(/\.[^.]+$/, '.jpeg');
-        res.sendFile(fileName, options, function (err) {
+
+    sendCached(){
+        var filepath = __dirname + "/images/" + this.fullfilename;
+        this.res.sendFile(filepath, this.options, function (err) {
             if (err) {
-                next(err)
+                this.next(err)
             } else {
-                console.log('Sent:', fileName);
+                console.log('Cache Hit :', filepath);
             }
         })
     }
-})
+
+    download(url, dest, callback){
+        var file = fs.createWriteStream(dest);
+        var protokoll = url.startsWith('https://') ? https : http;
+    
+        protokoll.get(url, function(response) {
+            response.pipe(file);
+            file.on('finish', function() {
+                file.close(callback);
+            });
+        });
+    }
+}
+
 
 app.use(express.static('images'))
 
